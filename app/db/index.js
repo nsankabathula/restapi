0d9b98b39aa5b233bbf2455faad020ef5a4239ba
nsankabathula <naveen.c.s@hotmail.com>
2018-01-30 11:32:16 -0500

    minor changes

--- app/db/index.js
+++ app/db/index.js
@@ -2,24 +2,24 @@ const loki = require('lokijs');
 
 module.exports =
 function () {
-    var db = new loki('./database.json',
+    var db = new loki('./data/database.json',
     {
         autoload:true,      
         autosave: true, 
         autosaveInterval: 4000,
         serializationMethod:'pretty',
-        verbose:true
-    }
-);
-databaseInitialize (db);
-return db;
-}
-
-function databaseInitialize(db) {    
+        verbose:true        
+    });
+    
     if (!db.getCollection("notes")) {
+        console.warn('Adding NOTES ..');
         var notesDb = db.addCollection('notes', {unique: ['title'], indices:['title','description']});
         notesDb.insert([{title:'title-1', description:'description-1'}, {title:'title-2', description:'description-2'}] );
-        }
+    }
+
+    console.info('db', db);
+    return db;
 }
+
 
 
